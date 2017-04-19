88b4fa5f1e7b6b4a030dc936657e68e7415dd1db
Chin Yan Keat (Digi - Central) <YKCHIN@DiGiCR.digi.com.my>
2017-04-19 09:44:38 +0800

    test server

--- chatbot/server.js
+++ chatbot/server.js
@@ -1837,13 +1837,14 @@ bot.beginDialogAction('weather', '/weather');   // <-- no 'matches' option means
 var server = restify.createServer();
 
 // Handle Bot Framework messages
-server.post('/api/messages', connector.listen());
+server.post('/chatbot/api/messages', connector.listen());
+//server.post('/api/messages', connector.listen());
 
-// Serve a static web page
-server.get(/.*/, restify.serveStatic({
-	'directory': './dso',
-	'default': 'digi.html'
-}));
+//// Serve a static web page
+//server.get(/.*/, restify.serveStatic({
+//	'directory': './dso',
+//	'default': 'digi.html'
+//}));
 
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
