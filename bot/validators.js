82da81f889c443549f1a449863f2b49c2d928429
Chin Yan Keat (Digi - Central) <YKCHIN@DiGiCR.digi.com.my>
2017-05-03 13:46:21 +0800

    fixed undefined log

--- bot/validators.js
+++ bot/validators.js
@@ -19,6 +19,24 @@ lib.dialog('email',
     builder.DialogAction.validatedPrompt(builder.PromptType.text, function (response) {
         return EmailRegex.test(response);
     }));
+  
+lib.dialog('otp',
+    builder.DialogAction.validatedPrompt(builder.PromptType.text, function (response) {
+console.log ("otp = " + session.userData.oneTimeCode);
+        return response == '1234';
+    }));
+//lib.dialog('otp', [
+//    function (session, args, next) {
+//        var otp = session.userData.oneTimeCode;
+//console.log("one time code = " + otp);
+//        builder.DialogAction.validatedPrompt({
+//            prompt: session.gettext('I have just sent the One Time Code to you. Can you please key in the 4 digit code?'),
+//            retryPrompt: session.gettext('Sorry, the code is incorrect. Let\'s try 1 more time'),
+//            maxRetries: 1
+//        }, function (response) {
+//            return response == '1234';
+//        })
+//    }]);
 
 // Export createLibrary() function
 module.exports.createLibrary = function () {
